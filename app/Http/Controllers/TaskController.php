32bd816c0f3954c7e5878a3939ca048dde2931a2
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2020-10-16 16:18:30 +0900

    add user auth

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,11 @@ use App\Category;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $categories = Category::all();
